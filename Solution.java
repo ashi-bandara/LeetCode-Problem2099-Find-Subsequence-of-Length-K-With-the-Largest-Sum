ede2668c645ac56e3185e8902b06b3f52b083b9c
Ashi Bandara <141470757+ashi-bandara@users.noreply.github.com>
2024-01-19 09:08:53 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,15 +1,1 @@
-import java.util.Arrays;
-class Solution {
-    public int[] maxSubsequence(int[] nums, int k) {
-        int[] numsClone = nums.clone();
-        int[] max = new int[k];
-        Arrays.sort(numsClone);
-
-        for (int i = 0, j = numsClone.length - 1; i < k && i < numsClone.length; i++, j--) {
-            max[i] = numsClone[j];
-        }
-
-        return max;
-    }
-}
 
